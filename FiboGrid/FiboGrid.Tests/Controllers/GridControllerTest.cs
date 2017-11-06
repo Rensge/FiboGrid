fa3ab6c6b5bba0eba90465a4a026b43dc75e6318
renske <rdasmicros@outlook.com>
2017-11-06 20:45:22 +0100

    initial working grid

---
+++ FiboGrid/FiboGrid.Tests/Controllers/GridControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FiboGrid;
+using FiboGrid.Controllers;
+
+namespace FiboGrid.Tests.Controllers
+{
+    [TestClass]
+    public class GridControllerTest
+    {
+        [TestMethod]
+        public void GetCellsTest()
+        {
+            // Arrange
+            GridController controller = new GridController();
+
+            // Act
+            JsonResult result = controller.GetCells() as JsonResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        
+    }
+}
---
+++ FiboGrid/FiboGrid/Models/FiboCheckResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FiboGrid.Models
+{
+    public class FiboCheckResult
+    {
+        public int Row { get; set; }
+
+        public List<int> FiboSerieCells { get; set; }
+        
+    }
+}
